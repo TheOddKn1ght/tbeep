efd4372f36a63553aec3ff0cb9a68d4ff2454761
Dmitry Sidelnikov <126893991+TheOddKn1ght@users.noreply.github.com>
2025-07-23 15:05:25 +0300

    add h:m:s; add countdown

--- tbeep.c
+++ tbeep.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <signal.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 #include "tbeep.h"
 
@@ -22,15 +24,73 @@ void write_temp_wav(const char *filename) {
     fclose(f);
 }
 
+int parse_duration(const char *str) {
+    int total_seconds = 0;
+    int value = 0;
+
+    while (*str) {
+        if (isdigit(*str)) {
+            value = value * 10 + (*str - '0');
+        } else {
+            switch (*str) {
+                case 'h': total_seconds += value * 3600; value = 0; break;
+                case 'm': total_seconds += value * 60; value = 0; break;
+                case 's': total_seconds += value; value = 0; break;
+                default:
+                    fprintf(stderr, "Invalid duration specifier: %c\n", *str);
+                    exit(EXIT_FAILURE);
+            }
+        }
+        str++;
+    }
+
+    if (value > 0) {
+        total_seconds += value;
+    }
+
+    return total_seconds;
+}
+
+void print_time_left(int seconds_left) {
+    int h = seconds_left / 3600;
+    int m = (seconds_left % 3600) / 60;
+    int s = seconds_left % 60;
+    if (h > 0)
+        printf("\rTime left: %02d:%02d:%02d   ", h, m, s);
+    else
+        printf("\rTime left: %02d:%02d   ", m, s);
+    fflush(stdout);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <seconds>\n", argv[0]);
+    bool verbose = false;
+    const char *duration_arg = NULL;
+
+    if (argc == 2) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            printf("Usage: %s [-v] <duration>\n", argv[0]);
+            printf(" (s)     -  seconds\n");
+            printf("  m      -  minutes\n");
+            printf("  h      -  hours \n");
+            printf("\nOptions:\n");
+            printf("  -v       Show countdown timer\n");
+            printf("  -h       Show this help message\n");
+            printf("\nPress Ctrl+C to stop the beep when time's up.\n");
+            return EXIT_SUCCESS;
+        } else {
+            duration_arg = argv[1];
+        }
+    } else if (argc == 3 && strcmp(argv[1], "-v") == 0) {
+        verbose = true;
+        duration_arg = argv[2];
+    } else {
+        fprintf(stderr, "Usage: %s [-v] <duration>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    int seconds = atoi(argv[1]);
+    int seconds = parse_duration(duration_arg);
     if (seconds <= 0) {
-        fprintf(stderr, "Please enter a positive number of seconds.\n");
+        fprintf(stderr, "Please enter a positive duration.\n");
         return EXIT_FAILURE;
     }
 
@@ -44,7 +104,16 @@ int main(int argc, char *argv[]) {
     write_temp_wav(tmp_wav);
 
     printf("Timer set for %d seconds...\n", seconds);
-    sleep(seconds);
+
+    if (verbose) {
+        for (int i = seconds; i > 0 && running; --i) {
+            print_time_left(i);
+            sleep(1);
+        }
+        printf("\r%*s\r", 30, "");
+    } else {
+        sleep(seconds);
+    }
 
     signal(SIGINT, handle_sigint);
     printf("Time's up! Beeping... (Ctrl+C to stop)\n");
@@ -57,7 +126,7 @@ int main(int argc, char *argv[]) {
         usleep(500000);
     }
 
-    unlink(tmp_wav); // Delete temp file
+    unlink(tmp_wav);
     printf("\nStopped.\n");
     return EXIT_SUCCESS;
 }
